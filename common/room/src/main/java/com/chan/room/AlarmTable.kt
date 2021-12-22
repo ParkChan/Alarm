37caf7200885fde96064aec21df5714db2d65a22
ParkChan <hydrarkk@naver.com>
2021-12-23 01:26:55 +0900

    [구현] - AlarmTable

---
+++ common/room/src/main/java/com/chan/room/AlarmTable.kt
@@ -0,0 +1,14 @@
+package com.chan.room
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "alarm")
+data class AlarmTable(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val alarmName: String,
+    val timeStamp: Long,
+    val isAlarm: Boolean,
+    val ringtoneUri: String,
+)
