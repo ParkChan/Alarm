1e7dd3a3795806a6bf78abc43ad6bfdd34a51942
ParkChan <hydrarkk@naver.com>
2021-12-27 04:39:39 +0900

    [수정] - Runtime Permission 다이얼로그 추가

--- app/src/main/java/com/chan/alarm/MainActivity.kt
+++ app/src/main/java/com/chan/alarm/MainActivity.kt
@@ -1,25 +1,31 @@
 package com.chan.alarm
 
+import android.app.AlertDialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings.ACTION_MANAGE_OVERLAY_PERMISSION
+import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContracts
 import com.chan.alarm.common.ui.SnackbarUtil
 import com.chan.alarm.databinding.ActivityMainBinding
 import com.chan.ui.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
+
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
 
     private val requestPermissionLauncher = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (isGranted) {
-            Timber.d("isGranted")
-        } else {
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        val overlaysPermission = Settings.canDrawOverlays(this)
+        if (!overlaysPermission) {
             SnackbarUtil.show(binding.root, getString(R.string.snack_bar_msg_runtime_permission))
+            showPermissionCheckDialog()
         }
+    }
+    private val dialogBuilder: AlertDialog.Builder by lazy {
+        AlertDialog.Builder(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +34,32 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     }
 
     private fun requestPermission() {
-        requestPermissionLauncher.launch(ACTION_MANAGE_OVERLAY_PERMISSION)
+        if (!Settings.canDrawOverlays(this)) {
+            showPermissionCheckDialog()
+        }
+    }
+
+    private fun showPermissionCheckDialog() {
+        dialogBuilder
+            .setMessage(
+                getString(
+                    R.string.dialog_message_overlay_permission
+                )
+            )
+            .setPositiveButton(
+                getString(R.string.dialog_message_ok)
+            ) { _, _ ->
+                val intent =
+                    Intent(
+                        Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:$packageName")
+                    )
+                requestPermissionLauncher.launch(intent)
+            }
+            .setNegativeButton(
+                getString(R.string.dialog_message_cancel)
+            ) { _, _ ->
+                finish()
+            }.create().show()
     }
 }
